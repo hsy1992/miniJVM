827183cd568f10e9ee7bfef1eb08f620026ea462
digitalgust <digitalgust@163.com>
2017-10-18 18:14:33 +0800

    remove comment

--- mini_jvm/utils/hashset.c
+++ mini_jvm/utils/hashset.c
@@ -10,9 +10,6 @@
 
 static s32 HASH_SET_DEFAULT_SIZE = 4;
 
-/* Internal function used to allocate the table on hash table creation
- * and when enlarging the table */
-
 static int hashset_allocate_table(Hashset *set, int size) {
     if (size) {
         set->table = jvm_alloc(size *
@@ -23,17 +20,8 @@ static int hashset_allocate_table(Hashset *set, int size) {
     return set->table != NULL;
 }
 
-/* Free an entry, calling the free functions if there are any registered */
 
 static void hashset_free_entry(Hashset *set, HashsetEntry *entry) {
-    /* If there is a function registered for freeing keys, use it to free
-     * the key */
-
-//    if (set->key_free_func != NULL) {
-//        set->key_free_func(entry->key);
-//    }
-
-    /* Free the data structure */
 
     jvm_free(entry);
 }
@@ -42,13 +30,8 @@ unsigned long _DEFAULT_HashsetHash(HashsetKey kmer) {
     return (unsigned long) kmer;
 }
 
-
-//Hashset *hashset_create(HashtableHashFunc hash_func,
-//                        HashtableEqualFunc equal_func) {
 Hashset *hashset_create() {
     Hashset *set;
-
-    /* Allocate a new hash table structure */
 
     set = (Hashset *) jvm_alloc(sizeof(Hashset));
 
@@ -56,12 +39,7 @@ Hashset *hashset_create() {
         return NULL;
     }
 
-//    set->hash_func = hash_func;
-//    set->equal_func = equal_func;
-//    set->key_free_func = NULL;
     set->entries = 0;
-
-    /* Allocate the table */
 
     if (!hashset_allocate_table(set, HASH_SET_DEFAULT_SIZE)) {
         jvm_free(set);
@@ -78,8 +56,6 @@ void hashset_destory(Hashset *set) {
     HashsetEntry *next;
     unsigned long long int i;
 
-    /* Free all entries in all chains */
-
     for (i = 0; i < set->table_size; ++i) {
         rover = set->table[i];
         while (rover != NULL) {
@@ -88,13 +64,7 @@ void hashset_destory(Hashset *set) {
             rover = next;
         }
     }
-
-    /* Free the table */
-
     jvm_free(set->table);
-
-    /* Free the hash table structure */
-
     jvm_free(set);
 }
 
@@ -122,39 +92,23 @@ void hashset_clear(Hashset *set) {
         }
     }
 }
-//
-//void hashset_register_free_functions(Hashset *set,
-//                                     HashtableKeyFreeFunc key_free_func) {
-//    set->key_free_func = key_free_func;
-//}
+
 
 int hashset_put(Hashset *set, HashsetKey key) {
     HashsetEntry *rover;
     HashsetEntry *newentry;
     unsigned long long int index;
 
-    /* If there are too many items in the table with respect to the table
-     * size, the number of hash collisions increases and performance
-     * decreases. Enlarge the table size to prevent this happening */
 
     if ((set->entries << 1) >= set->table_size) {
 
-        /* Table is more than 1/2 full */
-
         if (!hashset_resize(set, set->table_size << 1)) {
-
-            /* Failed to enlarge the table */
 
             return 0;
         }
     }
 
-    /* Generate the hash of the key and hence the index into the table */
-
     index = DEFAULT_HASH_FUNC(key) % set->table_size;
-
-    /* Traverse the chain at this location and look for an existing
-     * entry with the same key */
 
     rover = set->table[index];
 
@@ -174,16 +128,11 @@ int hashset_put(Hashset *set, HashsetKey key) {
 
     newentry->key = key;
     newentry->val = 0;
-    /* Link into the list */
 
     newentry->next = set->table[index];
     set->table[index] = newentry;
 
-    /* Maintain the count of the number of entries */
-
     ++set->entries;
-
-    /* Added successfully */
 
     return 1;
 }
@@ -197,28 +146,15 @@ HashsetKey hashset_get(Hashset *set, HashsetKey key) {
 HashsetEntry *hashset_get_entry(Hashset *set, HashsetKey key) {
     HashsetEntry *rover;
     unsigned long long int index;
-
-    /* Generate the hash of the key and hence the index into the table */
-
     index = DEFAULT_HASH_FUNC(key) % set->table_size;
-
-    /* Walk the chain at this index until the corresponding entry is
-     * found */
-
     rover = set->table[index];
 
     while (rover != NULL) {
         if (DEFAULT_HASH_EQUALS_FUNC(key, rover->key) != 0) {
-
-            /* Found the entry.  Return the data. */
-
             return rover;
         }
         rover = rover->next;
     }
-
-    /* Not found */
-
     return HASH_NULL;
 }
 
@@ -228,30 +164,15 @@ int hashset_remove(Hashset *set, HashsetKey key, int resize) {
     unsigned long long int index;
     unsigned long long int result;
 
-    /* If there are too few items in the table with respect to the table
-     * size, the table is taking up too much space. 
-     * Shrink table to improve space efficiency. */
+
 
     if (resize && (set->entries << 3) < set->table_size) {
-
-        /* Table is less than 1/8 full */
-
         if (!hashset_resize(set, set->table_size >> 1)) {
-
-            /* Failed to enlarge the table */
-
             return 0;
         }
     }
 
-    /* Generate the hash of the key and hence the index into the table */
-
     index = DEFAULT_HASH_FUNC(key) % set->table_size;
-
-    /* Rover points at the pointer which points at the current entry
-     * in the chain being inspected.  ie. the entry in the table, or
-     * the "next" pointer of the previous entry in the chain.  This
-     * allows us to unlink the entry when we find it. */
 
     result = 0;
     rover = &set->table[index];
@@ -259,30 +180,13 @@ int hashset_remove(Hashset *set, HashsetKey key, int resize) {
     while (*rover != NULL) {
 
         if (DEFAULT_HASH_EQUALS_FUNC(key, (*rover)->key) != 0) {
-
-            /* This is the entry to remove */
-
             entry = *rover;
-
-            /* Unlink from the list */
-
             *rover = entry->next;
-
-            /* Destroy the entry structure */
-
             hashset_free_entry(set, entry);
-
-            /* Track count of entries */
-
             --set->entries;
-
             result = 1;
-
             break;
         }
-
-        /* Advance to the next entry */
-
         rover = &((*rover)->next);
     }
 
@@ -297,13 +201,7 @@ void hashset_iterate(Hashset *set, HashsetIterator *iterator) {
     unsigned long long int chain;
 
     iterator->set = set;
-
-    /* Default value of next if no entries are found. */
-
     iterator->next_entry = NULL;
-
-    /* Find the first entry */
-
     for (chain = 0; chain < set->table_size; ++chain) {
 
         if (set->table[chain] != NULL) {
@@ -364,14 +262,10 @@ int hashset_resize(Hashset *set, int size) {
     unsigned long long int i;
 
     if (size != 0) {
-        /* Store a copy of the old table */
         old_table = set->table;
         old_table_size = set->table_size;
 
         if (!hashset_allocate_table(set, size)) {
-
-            /* Failed to allocate the new table */
-
             printf("CRITICAL: FAILED TO ALLOCATE HASH TABLE!\n");
 
             set->table = old_table;
@@ -379,32 +273,17 @@ int hashset_resize(Hashset *set, int size) {
 
             return 0;
         }
-
-        /* Link all entries from all chains into the new table */
-
         for (i = 0; i < old_table_size; ++i) {
             rover = old_table[i];
 
             while (rover != NULL) {
                 next = rover->next;
-
-                /* Find the index into the new table */
-
                 index = DEFAULT_HASH_FUNC(rover->key) % set->table_size;
-
-                /* Link this entry into the chain */
-
                 rover->next = set->table[index];
                 set->table[index] = rover;
-
-                /* Advance to next in the chain */
-
                 rover = next;
             }
         }
-
-        /* Free the old table */
-
         jvm_free(old_table);
     }
 
