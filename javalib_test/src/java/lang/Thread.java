b67aac94b92b6b862f8e3efb2322ba41760e333f
zhangpeng <gustzhang@yahoo.com>
2017-11-23 16:36:04 +0800

    test thread add stackframe var store runtime

--- javalib_test/src/java/lang/Thread.java
+++ javalib_test/src/java/lang/Thread.java
@@ -89,7 +89,8 @@ class Thread implements Runnable {
 
     /* Thread name */
     private char        name[];
-
+    /*  save top runtime   */
+    private long stackFrame;
     /**
      * The minimum priority that a thread can have.
      */
