742f6d33baebe81713b4a9ce12358bcf31d4a038
digitalgust <digitalgust@163.com>
2017-12-07 12:54:06 +0800

    test

--- javalib/src/test/Foo1.java
+++ javalib/src/test/Foo1.java
@@ -184,7 +184,7 @@ public class Foo1 {
     }
 
     void t7() {
-        int MAX = 100000;
+        int MAX = 10000;
         int PRINT_COUNT = 10000;
         Thread t = new Thread(new Runnable() {
             List<String> list = new ArrayList();
