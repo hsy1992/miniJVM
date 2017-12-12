8a7921630ac163e33098b29e25476635b0555385
digitalgust <digitalgust@163.com>
2017-12-12 13:24:55 +0800

    remove no use code

--- mini_jvm/test.c
+++ mini_jvm/test.c
@@ -20,60 +20,6 @@
 #include "jvm/garbage.h"
 
 
-void t1() {
-
-}
-
-void t2() {
-}
-
-void t3() {
-    LinkedList *list = linkedlist_create();
-    linkedlist_push_front(list, (__refer) (long) 1);
-    linkedlist_push_front(list, (__refer) (long) 2);
-    linkedlist_push_front(list, (__refer) (long) 3);
-    linkedlist_push_front(list, (__refer) (long) 4);
-    __refer ref;
-    while (NULL != (ref = linkedlist_pop_end(list))) {
-        printf("%lld\n", (s64) (long) ref);
-    }
-    linkedlist_destory(list);
-}
-
-void t4() {
-    printf("pointer size:%d\n", (s32) sizeof(void *));
-    printf("jint size:%d\n", (s32) sizeof(s32));
-    printf("jlong size:%d\n", (s32) sizeof(s64));
-    printf("jshort size:%d\n", (s32) sizeof(s16));
-    printf("jbyte size:%d\n", (s32) sizeof(c8));
-    printf("jdouble size:%d\n", (s32) sizeof(f64));
-    printf("jfloat size:%d\n", (s32) sizeof(f32));
-    printf("long size:%d\n", (s32) sizeof(long));
-}
-
-
-typedef struct _BB {
-    __refer ref;
-    s32 count;
-    autoptr *a;
-} BB;
-
-void t5() {
-    __refer r = jvm_calloc(100);
-    autoptr *a = autoptr_new(r);
-
-    BB *b = jvm_calloc(sizeof(BB));
-    b->a = autoptr_get(a);
-    autoptr_NULL(&a);
-    autoptr_NULL(&b->a);
-    jvm_printf("a = %llx , b->a = %llx\n", (s64) (long) a, (s64) (long) b->a);
-    jvm_free(b);
-}
-
-void _on_jvm_sig(s32 signo) {
-    int debug = 1;
-}
-
 /*
  *
  */
@@ -87,7 +33,6 @@ int main(int argc, char **argv) {
     s32 ret;
 //    ret = execute("../../javalib_test/build/classes/", "test/Foo1", argc, (c8 **)argv);
     ret = execute("../../javalib/build/classes/", "test/Foo1", argc, (c8 **) argv);
-//    t3();
 //    testMinizWrapper();
 
 
