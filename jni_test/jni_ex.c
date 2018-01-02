305b09f3c31c9eaded7762e678227dafe7ecf1bf
digitalgust <digitalgust@163.com>
2018-01-02 09:16:06 +0800

    jni test

--- jni_test/jni_ex.c
+++ jni_test/jni_ex.c
@@ -12,10 +12,16 @@ int test_JniTest_getValue(Runtime *runtime, Class *clazz) {
     env->push_int(runtime->stack, v + 1);
     return 0;
 }
+int test_JniTest_test(Runtime *runtime, Class *clazz) {
 
+    printf("native test_JniTest_test()V invoked: \n");
+
+    return 0;
+}
 
 static java_native_method method_test2_table[] = {
         {"test/JniTest", "getValue", "(I)I", test_JniTest_getValue},
+        {"test/JniTest", "test", "()V", test_JniTest_test},
 };
 
 void JNI_OnLoad(JniEnv *env) {
