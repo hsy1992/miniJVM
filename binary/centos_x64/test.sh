4e1d0763f97d375d3e00150033ada14336b9ec0b
digitalgust@163.com <digitalgust@163.com>
2018-03-13 06:42:50 -0400

    update linux bin

--- binary/centos_x64/test.sh
+++ binary/centos_x64/test.sh
@@ -1,2 +1,4 @@
 ./mini_jvm -cp ../minijvm_rt.jar:../minijvm_test.jar test/Foo1 888
 ./mini_jvm -cp ../minijvm_rt.jar:../jni_test.jar test/JniTest
+./mini_jvm -cp ../minijvm_rt.jar:../gui_lib.jar test/GuiTest
+./mini_jvm -cp ../minijvm_rt.jar:../gui_lib.jar test/Light
