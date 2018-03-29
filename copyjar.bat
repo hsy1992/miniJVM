c0f6f992cfabbd591a17233201792c338491f03a
zhangpeng <gustzhang@yahoo.com>
2018-03-29 18:24:18 +0800

    readme

--- copyjar.bat
+++ copyjar.bat
@@ -1,7 +1,7 @@
-copy jni_gui\java\dist\gui_lib.jar binary\lib\
-copy jni_test\java\dist\jni_test.jar binary\lib\
+copy ex_lib\jni_gui\java\dist\gui_lib.jar binary\lib\
+copy test\jni_test\java\dist\jni_test.jar binary\lib\
 copy javalib\dist\minijvm_rt.jar binary\lib\
-copy javalib_test\dist\minijvm_test.jar binary\lib\
+copy test\javalib_test\dist\minijvm_test.jar binary\lib\
 
 pause
 
--- copyjar.sh
+++ copyjar.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cp jni_gui/java/dist/gui_lib.jar binary/lib/
-cp jni_test/java/dist/jni_test.jar binary/lib/
+cp ex_lib/jni_gui/java/dist/gui_lib.jar binary/lib/
+cp test/jni_test/java/dist/jni_test.jar binary/lib/
 cp javalib/dist/minijvm_rt.jar binary/lib/
-cp javalib_test/dist/minijvm_test.jar binary/lib/
+cp test/javalib_test/dist/minijvm_test.jar binary/lib/
 
 
