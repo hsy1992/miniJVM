4f2603a7bd946746fc65e842a473f6c994c27a7f
digitalgust <digitalgust@163.com>
2018-04-07 11:45:27 +0800

    update lib

--- copyjar.sh
+++ copyjar.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-cp ex_lib/jni_gui/java/dist/gui_lib.jar binary/lib/
-cp test/jni_test/java/dist/jni_test.jar binary/lib/
 cp javalib/dist/minijvm_rt.jar binary/lib/
+
+cp ex_lib/jni_gui/java/dist/gui_lib.jar binary/lib/
+cp ex_lib/luaj/dist/luaj.jar binary/lib/
+
+cp test/jni_test/java/dist/jni_test.jar binary/lib/
 cp test/javalib_test/dist/minijvm_test.jar binary/lib/
 
 
