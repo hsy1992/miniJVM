2136edef2220f9acb2ac001da31a95f8dcc1653a
digitalgust <digitalgust@163.com>
2018-05-23 17:09:28 +0800

    fix vs atomic function

--- mini_jvm/utils/spinlock.c
+++ mini_jvm/utils/spinlock.c
@@ -9,12 +9,14 @@
 
 
 #if defined( __JVM_OS_VS__ )
-int __sync_bool_compare_and_swap(int *lock,int desired, int expected) {
-    return InterlockedCompareExchange(lock, expected, desired);
+int __sync_bool_compare_and_swap(volatile int *lock,int comparand, int exchange) {
+    //if *lock == comparand then *lock = exchange  and return old *lock value
+    if(InterlockedCompareExchange(lock, exchange, comparand) == comparand)return 1;
+    else return 0;
 }
 #endif
 
-int spin_init(spinlock_t volatile *lock, int pshared) {
+int spin_init(volatile spinlock_t *lock, int pshared) {
 //    __asm__ __volatile__ ("":: : "memory");
     *lock = 0;
     return 0;
@@ -25,10 +27,11 @@ int spin_destroy(spinlock_t *lock) {
 }
 
 
-inline int spin_lock_count(spinlock_t *lock, int count) {
+inline int spin_lock_count(volatile spinlock_t *lock, int count) {
     while (1) {
         int i;
         for (i = 0; i < count; i++) {
+            // if *lock == 0,then *lock = 1  ,  return true else return false
             if (__sync_bool_compare_and_swap(lock, 0, 1)) {
                 return 0;
             }
@@ -37,18 +40,18 @@ inline int spin_lock_count(spinlock_t *lock, int count) {
     }
 }
 
-int spin_lock(spinlock_t *lock) {
+int spin_lock(volatile spinlock_t *lock) {
     return spin_lock_count(lock, 100);
 }
 
-int spin_trylock(spinlock_t *lock) {
+int spin_trylock(volatile spinlock_t *lock) {
     if (__sync_bool_compare_and_swap(lock, 0, 1)) {
         return 0;
     }
     return 1;
 }
 
-int spin_unlock(spinlock_t volatile *lock) {
+int spin_unlock(volatile spinlock_t volatile *lock) {
     //__asm__ __volatile__ ("":: : "memory");
     *lock = 0;
     return 0;
--- mini_jvm/utils/spinlock.h
+++ mini_jvm/utils/spinlock.h
@@ -12,17 +12,17 @@
 
 typedef int spinlock_t;
 
-int spin_init(spinlock_t volatile *lock, int pshared);
+int spin_init(volatile spinlock_t *lock, int pshared);
 
 int spin_destroy(spinlock_t *lock);
 
-int spin_lock(spinlock_t *lock);
+int spin_lock(volatile spinlock_t *lock);
 
-int spin_lock_count(spinlock_t *lock, int count);
+int spin_lock_count(volatile spinlock_t *lock, int count);
 
-int spin_trylock(spinlock_t *lock);
+int spin_trylock(volatile spinlock_t *lock);
 
-int spin_unlock(spinlock_t volatile *lock);
+int spin_unlock(volatile spinlock_t *lock);
 
 
 #endif //MINI_JVM_SPINLOCK_H
