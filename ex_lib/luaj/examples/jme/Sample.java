2643e2e3d1133d26967cfd2dfd4d9132127a88db
digitalgust <digitalgust@163.com>
2018-06-08 14:11:24 +0800

    change xcode prj

--- ex_lib/luaj/examples/jme/Sample.java
+++ ex_lib/luaj/examples/jme/Sample.java
@@ -1,23 +1,28 @@
 
+import java.io.File;
 import org.luaj.vm2.*;
 import org.luaj.vm2.lib.jme.JmePlatform;
 
 public class Sample {
 
     public static void main(String[] args) {
-        //System.setProperty("luaj.package.path","./examples/lua/");
+        System.setProperty("luaj.package.path", "./res/");
         Sample sam = new Sample();
         sam.startApp();
     }
 
     // the script will be loaded as a resource 
-    private static final String DEFAULT_SCRIPT = "../../binary/macos/hello.lua";
+    private static final String DEFAULT_SCRIPT = "../res/hello.lua";
 
     protected void startApp() {
         // get the script as an app property
         String script = null;
         if (script == null) {
             script = DEFAULT_SCRIPT;
+            File f = new File(script);
+            if (!f.exists()) {
+                script = "./res/hello.lua";
+            }
         }
 
         // create an environment to run in
