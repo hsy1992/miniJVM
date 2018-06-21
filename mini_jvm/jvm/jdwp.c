fa2a2f11d80c15d5991679520bf0abb5f8cb2626
digitalgust <digitalgust@163.com>
2018-06-21 20:39:40 +0800

    fix jdwp get localvar frame bug

--- mini_jvm/jvm/jdwp.c
+++ mini_jvm/jvm/jdwp.c
@@ -2215,7 +2215,7 @@ s32 jdwp_client_process(JdwpClient *client, Runtime *runtime) {
                                 break;
                             }
                             case '8':
-                                l2d.l = localvar_getLong(runtime->localvar, slot);
+                                l2d.l = localvar_getLong(frame->localvar, slot);
 //                                l2d.i2l.i1 = localvar_getInt(frame->localvar, slot);
 //                                l2d.i2l.i0 = localvar_getInt(frame->localvar, slot + 1);
                                 vt.value = l2d.l;
