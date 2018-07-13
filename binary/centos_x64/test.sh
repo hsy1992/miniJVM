37f205627f3c5218ff1d15bdae64575495f159c9
Gust <digitalgust@163.com>
2018-07-13 19:52:19 +0800

    update linux binary

--- binary/centos_x64/test.sh
+++ binary/centos_x64/test.sh
@@ -1,4 +1,9 @@
-./mini_jvm -cp ../lib/minijvm_rt.jar:../lib/minijvm_test.jar test/Foo1 888
-./mini_jvm -cp ../lib/minijvm_rt.jar:../lib/jni_test.jar test/JniTest
+#./mini_jvm -Xmx100m -cp ../lib/minijvm_rt.jar:../lib/minijvm_test.jar test/Foo1 888
+#./mini_jvm -cp ../lib/minijvm_rt.jar:../lib/jni_test.jar test/JniTest
 ./mini_jvm -cp ../lib/minijvm_rt.jar:../lib/gui_lib.jar test/GuiTest
 ./mini_jvm -cp ../lib/minijvm_rt.jar:../lib/gui_lib.jar test/Light
+echo compile BpDeepTest.java
+./mini_jvm -cp ../lib/minijvm_rt.jar:../lib/janino.jar:../lib/commons-compiler.jar:../lib/ant-1.7,1.jar org.codehaus.janino.Compiler  ../res/BpDeepTest.java
+
+echo execute BpDeepTest
+./mini_jvm -cp ../lib/minijvm_rt.jar:../res/ BpDeepTest
