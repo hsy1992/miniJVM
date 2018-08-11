3cde10b3c6848e7a94b063913396750d5a73c73e
digitalgust <digitalgust@163.com>
2018-08-11 19:07:40 +0800

    update bat

--- binary/win32/test.bat
+++ binary/win32/test.bat
@@ -1,7 +1,7 @@
 mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/minijvm_test.jar test/Foo1 888
 mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/jni_test.jar test/JniTest
-mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/gui_lib.jar test/GuiTest
-mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/gui_lib.jar test/Light
+mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/glfw_gui.jar test/GuiTest
+mini_jvm.exe -cp ../lib/minijvm_rt.jar;../lib/glfw_gui.jar test/Light
 
 @echo compile BpDeepTest.java
 mini_jvm -cp ../lib/minijvm_rt.jar;../lib/janino.jar;../lib/commons-compiler.jar org.codehaus.janino.Compiler  ../res/BpDeepTest.java
